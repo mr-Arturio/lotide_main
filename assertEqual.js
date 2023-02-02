0b47bb7787b5e6d52e56d9d2559dd0f8129b9ee4
ArturT <mr.arturio@gmail.com>
2023-02-01 23:13:26 -0500

    fixing my repository

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  console.log((actual === expected) ? `🟢🟢🟢Assertion passed ${actual} === ${expected}` : `🔴🔴🔴Assertion Failed ${actual} !== ${expected}`);
+}; // Our assertEqual function can only compare primitive values.
+
+
+// TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('Blue', 'blue');
+assertEqual('red', 'red');
+assertEqual(!true, false);
