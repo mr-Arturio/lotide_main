08cf531d24ebcc138f6d68a622374d3cccc180dc
ArturT <mr.arturio@gmail.com>
2023-02-01 23:40:42 -0500

    ternary operator used

---
+++ findKeyByValue.js
@@ -0,0 +1,21 @@
+const assertEqual = (actual, expected) => {
+  console.log((actual === expected) ? `🟢🟢🟢Assertion passed ${actual} === ${expected}` : `🔴🔴🔴Assertion Failed ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key of Object.keys(object)) {
+    return (object[key] === value) ? key : undefined;
+    
+}
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
