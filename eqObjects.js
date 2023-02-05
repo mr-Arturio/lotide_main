dcd7e64ac4041d3702db01134bab3c81f444a2dc
ArturT <mr.arturio@gmail.com>
2023-02-05 17:45:52 -0500

    comments added

--- eqObjects.js
+++ eqObjects.js
@@ -16,12 +16,12 @@ const eqArrays = (arr1, arr2) => {
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
   // const oneShirt = Object.keys(object1);
-  // const anotherShirt = Object.keys(object2);
+  // const anotherShirt = Object.keys(object2); //by tgis line, we store arrays in variable(anotherShirt...) and we can use for ..of loop.
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
 
-  for (const key in object1) {
+  for (const key in object1) {//for..of loop works only with arrays, not objects.
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
