511ffb8f20a69f3f75eeccc3b502c4020d2fe5ed
ArturT <mr.arturio@gmail.com>
2023-02-05 18:12:53 -0500

    finished

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,24 +1,23 @@
-const eqObjects = function (object1, object2) {
-  const oneShirt = Object.keys(object1);
-  const anotherShirt = Object.keys(object2);
-  if (oneShirt.length === anotherShirt.length) {
-    return true;
+const eqObjects = (object1, object2) => {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
   }
 
-  for (let key of oneShirt) {
-    if (oneShirt[key] !== anotherShirt[key]) {
-      return false;
-    }
-    return true;
-  }
-  //return false;
+  //   for (const key in object1) {
+  //     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+  //       if (!eqArrays(object1[key], object2[key])) {
+  //         return false;
+  //       }
+  //     } else if (object1[key] !== object2[key]) {
+  //       return false;
+  //     }
+  //   }
+  return true;
 };
 
-const assertObjectsEqual = function (actual, expected) {
-  const inspect = require('util').inspect; // <= add this line
-  // ...
+const assertObjectsEqual = (actual, expected) => {
+  const inspect = require('util').inspect;
   return (eqObjects(actual, expected)) ? `🟢🟢🟢Assertion passed:  ${inspect(actual)} === ${inspect(expected)}` : `🔴🔴🔴Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
-
 };
 
-//console.log(`Example label: ${inspect(actual)}`);
+console.log(assertObjectsEqual({ a: "1", b: 2, }, { b: 2, a: "1" }));
