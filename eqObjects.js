626b00de2284a9b8eb254895837e8e7f731754e4
ArturT <mr.arturio@gmail.com>
2023-02-05 17:43:08 -0500

    eqArrays fixed + other fixing

--- eqObjects.js
+++ eqObjects.js
@@ -15,19 +15,22 @@ const eqArrays = (arr1, arr2) => {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-  const oneShirt = Object.keys(object1);
-  const anotherShirt = Object.keys(object2);
-  if (oneShirt.length === anotherShirt.length) {
-    return true;
+  // const oneShirt = Object.keys(object1);
+  // const anotherShirt = Object.keys(object2);
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
   }
 
-  for (let key of oneShirt) {
-    if (oneShirt[key] !== anotherShirt[key]) {
+  for (const key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
-    return true;
   }
-  //return false;
+  return true;
 };
 
 const shirtObject = {
@@ -63,7 +66,20 @@ const longSleeveMultiColorShirtObject = {
   sleeveLength: "long"
 };
 
+const myPet = {
+  breed: 'dog',
+  color: 'black',
+  age: 7
+};
+
+const myPet2 = {
+  breed: 'dog',
+  color: 'black',
+  age: 7
+};
+
 console.log(assertEqual(eqObjects(shirtObject, anotherShirtObject), true)); // => true
 console.log(assertEqual(eqObjects(shirtObject, longSleevShirtObject), false)); // => false
 console.log(assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false));
 console.log(assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true));
+console.log(assertEqual(eqObjects(myPet, myPet2), false));
