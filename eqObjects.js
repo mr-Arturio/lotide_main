3cc3003baa92b59b458a6d42f1b4b5afc58ac337
ArturT <mr.arturio@gmail.com>
2023-02-05 17:53:10 -0500

    shorter function

--- eqObjects.js
+++ eqObjects.js
@@ -14,7 +14,7 @@ const eqArrays = (arr1, arr2) => {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
+const eqObjects = (object1, object2) => {
   // const oneShirt = Object.keys(object1);
   // const anotherShirt = Object.keys(object2); //by tgis line, we store arrays in variable(anotherShirt...) and we can use for ..of loop.
   if (Object.keys(object1).length !== Object.keys(object2).length) {
