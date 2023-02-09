2e9d99fdd06b65435d56b0f0c39c39f6d3c6db87
ArturT <mr.arturio@gmail.com>
2023-02-09 17:29:13 -0500

    require added

--- letterPositions.js
+++ letterPositions.js
@@ -1,19 +1,4 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (actual, expected) => {
-  console.log((eqArrays(actual, expected)) ? `🟢🟢🟢Assertion passed ${actual} === ${expected}` : `🔴🔴🔴Assertion Failed ${actual} !== ${expected}`);
-};
-
+const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function (sentence) {
   const results = {};  //empty variable that will show/store the position of leter in the sentence
