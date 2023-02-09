7e30b8d6668d76e8c620d49f16f07d7c30be136e
ArturT <mr.arturio@gmail.com>
2023-02-09 17:28:39 -0500

    module.export added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,6 +13,8 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (actual, expected) => (eqArrays(actual, expected)) ? `🟢🟢🟢Assertion Passed ${actual} === ${expected}` : `🔴🔴🔴Assertion Failed ${actual} !== ${expected}`;
 
+module.exports = assertArraysEqual;
+
 /*const assertArraysEqual = (array1, array2) => {
   if (eqArrays(array1, array2)) {
     console.log("equal");
@@ -22,9 +24,8 @@ const assertArraysEqual = (actual, expected) => (eqArrays(actual, expected)) ? `
     return false;
   }
 } */
-
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 'test'], [1, 2, 'test']));
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3, 4]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 'test'], [1, 2, 'test']);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
