6ea62c701ec49c1eed99a9810b4e2268bb298b11
ArturT <mr.arturio@gmail.com>
2023-02-14 21:13:03 -0500

    refactored

--- head.js
+++ head.js
@@ -5,11 +5,14 @@ const head = function (arr) {
   return (arr[0]);
 };
 
+module.exports = head;
+
+
 
 // TEST CODE
-// console.log(assertEqual(head([5,6,7]), 5));
-// console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-// console.log(assertEqual(head([5]), 5));
-// console.log(assertEqual(head([])));
-// console.log(assertEqual(head([3]), 2));
-// console.log(assertEqual(head([3], [3, 6]), 3));
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5]), 5);
+// assertEqual(head([]));
+// assertEqual(head([3]), 2);
+// assertEqual(head([3], [3, 6]), 3);
