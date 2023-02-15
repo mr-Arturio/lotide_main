94eba2ee8fb78803ff697cece08ad2a15f26fc64
ArturT <mr.arturio@gmail.com>
2023-02-14 20:24:27 -0500

    minor fixing

--- assertEqual.js
+++ assertEqual.js
@@ -3,8 +3,8 @@ const assertEqual = (actual, expected) => (actual === expected) ? `🟢🟢🟢A
 module.exports = assertEqual;
 
 // TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual('Blue', 'blue');
-assertEqual('red', 'red');
-assertEqual(!true, false);
+// console.log(assertEqual('Lighthouse Labs', 'Bootcamp'));
+// console.log(assertEqual(1, 1));
+// assertEqual('Blue', 'blue');
+// assertEqual('red', 'red');
+// assertEqual(!true, false);
