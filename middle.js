0c3d31f844364b67b188849170bea10083a2afc7
ArturT <mr.arturio@gmail.com>
2023-02-15 18:31:18 -0500

    refactored

--- middle.js
+++ middle.js
@@ -1,6 +1,3 @@
-
-const assertArraysEqual = require('./assertArraysEqual.js')
-
 const middle = (array) => {
   if (array.length <= 2) {
     return [];
@@ -13,15 +10,4 @@ const middle = (array) => {
 
 };
 
-/*console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4] */
-assertArraysEqual(middle([1]), ([]));
-assertArraysEqual(middle([1, 2]), ([])); // => []
-assertArraysEqual(middle([1, 2, 3]), ([2])); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), ([3])); // => [3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3]))); // => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), ([3, 4]))); // => [3, 4]*/
+module.exports = middle;
