367a7976fc18c392b4d0e016df62c1f35cf9712b
ArturT <mr.arturio@gmail.com>
2023-02-14 23:28:38 -0500

    refactored, require + exports

--- tail.js
+++ tail.js
@@ -1,12 +1,12 @@
-const assertEqual = (actual, expected) => {
-  console.log((actual === expected) ? `🟢🟢🟢Assertion passed ${actual} === ${expected}` : `🔴🔴🔴Assertion Failed ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
