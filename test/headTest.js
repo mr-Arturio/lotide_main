5c578f237e53f07e07a0ec455269564cc8fdffeb
ArturT <mr.arturio@gmail.com>
2023-02-16 17:42:31 -0500

    mocha headTest

---
+++ test/headTest.js
@@ -0,0 +1,22 @@
+
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1)
+  });
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(["5"]), "5");
+  })
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  })
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([]), );
+  })
+  it('returns 3 for ([3], [3, 6])', () => {
+    assert.strictEqual(head([3], [3, 6]), 3);
+  })
+  
+});
