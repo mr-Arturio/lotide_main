5c42f6c18b93fcbb80ec4692dd49a4aea3ed1590
ArturT <mr.arturio@gmail.com>
2023-02-16 19:25:44 -0500

    clean

--- tail.js
+++ tail.js
@@ -5,8 +5,3 @@ const tail = function(array) {
 };
 
 module.exports = tail;
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
