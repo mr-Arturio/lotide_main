5ec20c5a58aa89d3d1c84d2259e8feb4601f08b5
ArturT <mr.arturio@gmail.com>
2023-02-16 19:02:12 -0500

    refactored + fixed

--- without.js
+++ without.js
@@ -1,54 +1,25 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
-/*const assertArraysEqual = (actual, expected) => {
-  console.log((actual, expected) ? `🟢🟢🟢Assertion passed ${actual} === ${expected}` : `🔴🔴🔴Assertion Failed ${actual} !== ${expected}`);
-}; */
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log("equal");
-    return true;
-  } else {
-    console.log("not equal");
-    return false;
-  }
-};
 
-const without = (source, itemsToRemove) => {
-  let final = [];
+function without(source, itemsToRemove) {
+  const final = [];
   for (let i = 0; i < source.length; i++) {
-    if (source[i] === itemsToRemove[i]) {
-      //console.log(itemsToRemove[i])
-    } else {
+    let shouldRemove = false;
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        shouldRemove = true;
+        break;
+      }
+    }
+    if (!shouldRemove) {
       final.push(source[i]);
     }
   }
-  //console.log(final)
   return final;
 };
 
-/*const without = (source, itemsToRemove) => {
-  return source.filter((item) => {
-    return !itemsToRemove.includes(item);
-  });
-}; */
+// const without = (source, itemsToRemove) => { 
+//   return source.filter((element) => !itemsToRemove.includes(element));
+// };
 
-// TEST CODE
-//without([1, 2, 3], [1]); // => [2, 3]
-const test = ["1", "2", "3"];
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-assertArraysEqual(test, ["1", "2", "3"]);
-
-const words = ["hello", "world", "lighthouse"];
-console.log(without(["hello", "world", "lighthouse"], ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
