3dfb200324958bdeaaf805857a4890886bb6656b
ArturT <mr.arturio@gmail.com>
2023-02-17 23:08:47 -0500

    refactored

--- map.js
+++ map.js
@@ -1,16 +1,14 @@
-const assertArraysEqual = require('./assertArraysEqual')
-
+//const assertArraysEqual = require('./assertArraysEqual')
 
 const map = (array, callback) => {
   const results = [];
   for (let item of array) {
-    results.push(callback(item)); // why (), not []??
-  }
+    results.push(callback(item)); // why (), not []?? - [] to access an element from an array, () to call a function
+  }                              //'item' is not an array, it's a single element from the array parameter that was passed into the map function.
   return results;
 };
 
-const number = [10, 7, 23, 100];
-const subtraction = num => num - 7; //function(num){ return num - 7}
-const resultNumber = map(number, subtraction);
-console.log(assertArraysEqual(resultNumber, [3, 0, 16, 93])); //  true
+module.exports = map;
+
+
 
