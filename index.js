c3b9dc30dfb174c55e5d96618cacf2816994804c
ArturT <mr.arturio@gmail.com>
2023-02-17 20:46:52 -0500

    Exporting Objects

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const fflatten = require('./flatten');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten
+}
