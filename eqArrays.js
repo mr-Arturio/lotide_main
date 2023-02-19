1661de1e9754ba9a2e057ada3e3179e625761de0
ArturT <mr.arturio@gmail.com>
2023-02-18 21:31:00 -0500

    comments edited

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,9 @@
-const assertEqual = require("./assertEqual.js");
-
 const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
+  if (arr1.length !== arr2.length) { //checks if length not equal
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+  for (let i = 0; i < arr1.length; i++) { //if length equal, checks if array matches
+    if (arr1[i] !== arr2[i]) { 
       return false;
     }
   }
