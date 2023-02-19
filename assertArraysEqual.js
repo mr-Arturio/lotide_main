760bd24643f0a1b775e0832067b30536d5cfe856
ArturT <mr.arturio@gmail.com>
2023-02-18 21:16:10 -0500

    unused removed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,18 +7,3 @@ const assertArraysEqual = (actual, expected) => {
   : console.log(`🔴🔴🔴Assertion Failed ${actual} !== ${expected}`);
 }
 module.exports = assertArraysEqual;
-
-/*const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log("equal");
-    return true;
-  } else {
-    console.log("not equal");
-    return false;
-  }
-} */
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 'test'], [1, 2, 'test']);
-// assertArraysEqual([1, 2, 3], [3, 2, 1]);
-// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
