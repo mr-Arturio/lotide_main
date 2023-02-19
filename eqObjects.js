24eb9a53900dc9e49e4ea838cf3e675a13375e0c
ArturT <mr.arturio@gmail.com>
2023-02-18 21:32:21 -0500

    unused lines deleted

--- eqObjects.js
+++ eqObjects.js
@@ -1,10 +1,6 @@
 const eqArrays = require('./eqArrays');
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = (object1, object2) => {
-  // const oneShirt = Object.keys(object1);
-  // const anotherShirt = Object.keys(object2); //by tgis line, we store arrays in variable(anotherShirt...) and we can use for ..of loop.
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
