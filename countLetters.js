a299b2924cb6d81516381916bc54a431e8e384d1
ArturT <mr.arturio@gmail.com>
2023-02-18 21:24:18 -0500

    comments added

--- countLetters.js
+++ countLetters.js
@@ -1,15 +1,15 @@
 const countLetters = function (text) {
-  let quantity = {}; // why {}??
+  let quantity = {};
 
   for (let i = 0; i < text.length; i++) {
 
     let letter = text[i];
-    if (letter !== ' ') {
+    if (letter !== ' ') { //check if the character is space, if not move forward
 
-      if (!quantity[letter]) {
-        quantity[letter] = 1;
+      if (!quantity[letter]) { // checks if character is already present in the quantity object
+        quantity[letter] = 1; //if not present, new property added with value 1
       } else {
-        quantity[letter]++;
+        quantity[letter]++;// if present, count increase by 1
       }
 
     }
