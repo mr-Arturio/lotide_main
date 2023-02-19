655a3449f739a5d41907c9fa8b6b549310bb3445
ArturT <mr.arturio@gmail.com>
2023-02-18 21:40:40 -0500

    unused deleted

--- head.js
+++ head.js
@@ -1,18 +1,6 @@
-const assertEqual = require('./assertEqual');
-
 const head = function (arr) {
- 
+
   return (arr[0]);
 };
 
 module.exports = head;
-
-
-
-// TEST CODE
-//assertEqual(head([5,6,7]), 5);
-//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([5]), 5);
-// assertEqual(head([]));
-// assertEqual(head([3]), 2);
-// assertEqual(head([3], [3, 6]), 3);
