bbcb4dda4d0149d801715a277d8b7c4f78185943
ArturT <mr.arturio@gmail.com>
2023-02-18 21:47:13 -0500

    comments edited

--- letterPositions.js
+++ letterPositions.js
@@ -1,20 +1,18 @@
-//const assertArraysEqual = require('./assertArraysEqual');
-
 const letterPositions = function (sentence) {
   const results = {};  //empty variable that will show/store the position of leter in the sentence
-  for (let i = 0; i < sentence.length; i++) { //looping through the sentence, checking every symbol
-    let letter = sentence[i]; // creating new var letter and giving it a value of every symbol in sentance
-    if (letter !== ' ') { // skipping execution fn the " " spaces by checking if symbol NOT = " "
+  
+  for (let i = 0; i < sentence.length; i++) { //checking every symbol
+    let letter = sentence[i]; //  new var = every symbol in sentance
+    
+    if (letter !== ' ') { // skipping execution if symbol NOT = " "
       if (results[sentence[i]]) { // if its not a space we check if object already exist in the the results - skiping for the 'first time' letters.
-        //console.log(results[sentence[i]].push(i));
         results[sentence[i]].push(i);  //  we push to the end of string current index(i) of all sybols that repeats... if letter/symbol duplicate
       } else { // here we printing positions(i) of all unique letters/symbols
         results[sentence[i]] = [i]; //  results of sentence objects = number of position
-        //console.log(results[sentence[i]] = [i]);
+
       }
     }
   }
-  //  console.log(results);
   return results;
 };
 
