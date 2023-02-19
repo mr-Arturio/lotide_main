9e0ce9eb4fa052b37479660d87c73761ab2c2ff0
ArturT <mr.arturio@gmail.com>
2023-02-18 21:48:35 -0500

    unused code deleted

--- map.js
+++ map.js
@@ -1,10 +1,8 @@
-//const assertArraysEqual = require('./assertArraysEqual')
-
 const map = (array, callback) => {
   const results = [];
   for (let item of array) {
-    results.push(callback(item)); // why (), not []?? - [] to access an element from an array, () to call a function
-  }                              //'item' is not an array, it's a single element from the array parameter that was passed into the map function.
+    results.push(callback(item)); 
+    }                              
   return results;
 };
 
