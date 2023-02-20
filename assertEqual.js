b6d6a9480a31a988b237a5701affa6ceea8e66ea
ArturT <mr.arturio@gmail.com>
2023-02-20 10:10:14 -0500

    remove unused code

--- assertEqual.js
+++ assertEqual.js
@@ -5,10 +5,3 @@ const assertEqual = (actual, expected) => {
   : console.log(`🔴🔴🔴Assertion Failed ${actual} !== ${expected}`);
 } // Our assertEqual function can only compare primitive values.
 module.exports = assertEqual;
-
-// TEST CODE
-// assertEqual('Lighthouse Labs', 'Bootcamp');
-// assertEqual(1, 1);
-// assertEqual('Blue', 'blue');
-// assertEqual('red', 'red');
-// assertEqual(!true, false);
